b23fde1e270bacd3d081e6fc8e11c0bb65bbbbdf
Murat Atimtay <atimtay@gmail.com>
2025-03-13 14:37:12 +0300

    auto_remove_ordinalscom

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,61 @@
-import { defineConfig } from "vite"
-import { viteSingleFile } from "vite-plugin-singlefile"
+// import { defineConfig } from "vite"
+// import { viteSingleFile } from "vite-plugin-singlefile"
 
-const noAttr = () => {
-	return {
-	  name: "no-attribute",
-	  transformIndexHtml(html) {
-		return html.replace(`type="module" crossorigin`, "");
-	  }
-	}
-  }
+// const noAttr = () => {
+// 	return {
+// 	  name: "no-attribute",
+// 	  transformIndexHtml(html) {
+// 		return html.replace(`type="module" crossorigin`, "");
+// 	  }
+// 	}
+//   }
   
-  server: {
-    cors: {
-        origin: false
+//   server: {
+//     cors: {
+//         origin: false
+//     }
+// }
+
+// export default defineConfig({
+// 	// plugins: [viteSingleFile(),noAttr()],
+// 	plugins: [viteSingleFile()],
+// 	build: {
+// 		target:'esnext',
+// 		polyfillModulePreload: false,
+// 		rollupOptions: {
+// 		  output: {
+// 			interop: false, // Disable interop helpers that may add extra snippets
+// 		  },
+// 		},
+		
+// 	},
+// })
+
+
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+const removeOrdinals = () => {
+  return {
+    name: "remove-ordinals",
+    transformIndexHtml(html) {
+      return html.replace(
+        /<script\s+src="https:\/\/ordinals\.com\/content\/([^"]+)"><\/script>/g,
+        '<script src="/content/$1"></script>'
+      );
     }
-}
+  };
+};
 
 export default defineConfig({
-	// plugins: [viteSingleFile(),noAttr()],
-	plugins: [viteSingleFile()],
-	build: {
-		target:'esnext',
-		polyfillModulePreload: false,
-		rollupOptions: {
-		  output: {
-			interop: false, // Disable interop helpers that may add extra snippets
-		  },
-		},
-	  },
-})
+  plugins: [viteSingleFile(), removeOrdinals()],
+  build: {
+    target: "esnext",
+    polyfillModulePreload: false,
+    rollupOptions: {
+      output: {
+        interop: false, // Disable interop helpers that may add extra snippets
+      },
+    },
+  },
+});
