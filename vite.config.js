9fcf029148aaf97b23e26496b91f5930a75315d7
Murat Atimtay <atimtay@gmail.com>
2025-03-13 23:20:28 +0300

    Auto_clean

--- vite.config.js
+++ vite.config.js
@@ -1,66 +1,3 @@
-// import { defineConfig } from "vite"
-// import { viteSingleFile } from "vite-plugin-singlefile"
-
-// const noAttr = () => {
-// 	return {
-// 	  name: "no-attribute",
-// 	  transformIndexHtml(html) {
-// 		return html.replace(`type="module" crossorigin`, "");
-// 	  }
-// 	}
-//   }
-  
-//   server: {
-//     cors: {
-//         origin: false
-//     }
-// }
-
-// export default defineConfig({
-// 	// plugins: [viteSingleFile(),noAttr()],
-// 	plugins: [viteSingleFile()],
-// 	build: {
-// 		target:'esnext',
-// 		polyfillModulePreload: false,
-// 		rollupOptions: {
-// 		  output: {
-// 			interop: false, // Disable interop helpers that may add extra snippets
-// 		  },
-// 		},
-		
-// 	},
-// })
-
-
-// import { defineConfig } from "vite";
-// import { viteSingleFile } from "vite-plugin-singlefile";
-
-// const removeOrdinals = () => {
-//   return {
-//     name: "remove-ordinals",
-//     transformIndexHtml(html) {
-//       return html.replace(
-//         /<script\s+src="https:\/\/ordinals\.com\/content\/([^"]+)"><\/script>/g,
-//         '<script src="/content/$1"></script>'
-//       );
-//     }
-//   };
-// };
-
-// export default defineConfig({
-//   plugins: [viteSingleFile(), removeOrdinals()],
-//   build: {
-//     target: "esnext",
-//     polyfillModulePreload: false,
-//     rollupOptions: {
-//       output: {
-//         interop: false, // Disable interop helpers that may add extra snippets
-//       },
-//     },
-//   },
-// });
-
-
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
@@ -75,11 +12,11 @@ const cleanHTML = () => {
           '<script src="/content/$1"></script>'
         )
         // Remove the testing code block
-        .replace(/\/\/ THIS PART IS FOR TESTING ONLY[\s\S]*?\/\/ TESTING END/, "")
+        .replace(/\/\/ THIS PART IS FOR TESTING ONLY[\s\S]*?\/\/ TESTING END\s*/, "")
         // Uncomment the inscription_id line
         .replace(
-          /\/\/__________UNCOMMENT BELOW BEFORE INSCRIBING____________________\n\s*\/\/(let inscription_id = window\.location\.pathname\.split\("\/"\)\.pop\(\);)\n\s*\/\/_________UMCOMMENT ABOVE BEFORE INSCRIBING______________________/,
-          "$1"
+          /\/\/let inscription_id = window\.location\.pathname\.split\("\/"\)\.pop\(\);/,
+          "let inscription_id = window.location.pathname.split(\"/\").pop();"
         );
     }
   };
